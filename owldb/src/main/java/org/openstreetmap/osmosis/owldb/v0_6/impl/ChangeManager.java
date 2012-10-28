45a9ea5bd33cd5f3fdd994fe314bb758161cb058
Paweł Paprota <ppawel@fastmail.fm>
2012-10-28 20:22:45 +0100

    Changes for the latest OWL schema; simplified implementation.

--- owldb/src/main/java/org/openstreetmap/osmosis/owldb/v0_6/impl/ChangeManager.java
+++ owldb/src/main/java/org/openstreetmap/osmosis/owldb/v0_6/impl/ChangeManager.java
@@ -3,6 +3,7 @@ package org.openstreetmap.osmosis.owldb.v0_6.impl;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.logging.Logger;
 
 import org.openstreetmap.osmosis.core.database.FeaturePopulator;
 import org.openstreetmap.osmosis.core.domain.v0_6.Entity;
+import org.openstreetmap.osmosis.core.domain.v0_6.EntityType;
 import org.openstreetmap.osmosis.core.domain.v0_6.Node;
 import org.openstreetmap.osmosis.core.domain.v0_6.Relation;
 import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
@@ -21,14 +23,19 @@ import org.openstreetmap.osmosis.owldb.common.DatabaseContext;
 import org.openstreetmap.osmosis.owldb.common.PointBuilder;
 import org.postgis.PGgeometry;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.util.StringUtils;
 
 
 public class ChangeManager {
 	private static final Logger LOG = Logger.getLogger(ChangeManager.class.getName());
 
-	private final static String INSERT_SQL = "INSERT INTO changes "
-			+ "(user_id, version, changeset_id, tstamp, action, change_type, el_type, el_id, tags, new_tags, geom, new_geom) VALUES "
-			+ "(?, ?, ?, ?, ?::action, ?::change_type, ?::element_type, ?, ?, ?, ?, ?)";
+	private final static String[] INSERT_COLUMNS = new String[] { "el_type", "el_id", "version", "changeset_id",
+			"tstamp", "action", "changed_tags", "changed_geom", "changed_members", "current_tags", "new_tags",
+			"current_geom", "new_geom" };
+
+	private final static String INSERT_SQL = "INSERT INTO changes " + "("
+			+ StringUtils.arrayToCommaDelimitedString(INSERT_COLUMNS) + ") VALUES "
+			+ "(?::element_type, ?, ?, ?, ?, ?::action, ?, ?, ?, ?, ?, ?, ?)";
 
 	private SimpleJdbcTemplate jdbcTemplate;
 	private PreparedStatement changeInsertStatement;
@@ -79,70 +86,116 @@ public class ChangeManager {
 
 
 	protected void processCommon(ChangeAction action, Entity existingEntity, Entity newEntity) throws SQLException {
-		changeInsertStatement.setInt(1, newEntity.getUser().getId());
-		changeInsertStatement.setInt(2, newEntity.getVersion());
-		changeInsertStatement.setLong(3, newEntity.getChangesetId());
-		changeInsertStatement.setTimestamp(4, new Timestamp(newEntity.getTimestamp().getTime()));
-		changeInsertStatement.setString(5, action.toString().toUpperCase());
-		changeInsertStatement.setString(6, "CREATE");
-		changeInsertStatement.setString(7, newEntity.getType().toString().substring(0, 1));
-		changeInsertStatement.setLong(8, newEntity.getId());
-		changeInsertStatement.setObject(9, null);
-		changeInsertStatement.setObject(10, getTags(newEntity));
-		changeInsertStatement.setObject(11, null);
-		changeInsertStatement.setObject(12, null);
+		changeInsertStatement.setString(getParamIndex("el_type"), newEntity.getType().toString().substring(0, 1));
+		changeInsertStatement.setLong(getParamIndex("el_id"), newEntity.getId());
+		changeInsertStatement.setInt(getParamIndex("version"), newEntity.getVersion());
+		changeInsertStatement.setLong(getParamIndex("changeset_id"), newEntity.getChangesetId());
+		changeInsertStatement.setTimestamp(getParamIndex("tstamp"), new Timestamp(newEntity.getTimestamp().getTime()));
+		changeInsertStatement.setString(getParamIndex("action"), action.toString().toUpperCase());
+		changeInsertStatement.setBoolean(getParamIndex("changed_tags"), false);
+		changeInsertStatement.setBoolean(getParamIndex("changed_geom"), false);
+		changeInsertStatement.setBoolean(getParamIndex("changed_members"), false);
+
+		switch (action) {
+		case Create:
+			changeInsertStatement.setObject(getParamIndex("new_geom"), getGeom(newEntity));
+			changeInsertStatement.setObject(getParamIndex("new_tags"), new PGHStore(getTags(newEntity)));
+			changeInsertStatement.setBoolean(getParamIndex("changed_tags"), true);
+			changeInsertStatement.setBoolean(getParamIndex("changed_geom"), true);
+			changeInsertStatement.setBoolean(getParamIndex("changed_members"), newEntity.getType() != EntityType.Node);
+			break;
+
+		case Modify:
+			Map<String, String> currentTags = getTags(existingEntity);
+			Map<String, String> newTags = getTags(newEntity);
+
+			if (existingEntity != null) {
+				changeInsertStatement.setObject(getParamIndex("current_geom"), getGeom(existingEntity));
+				changeInsertStatement.setObject(getParamIndex("current_tags"), new PGHStore(currentTags));
+			}
+
+			changeInsertStatement.setObject(getParamIndex("new_geom"), getGeom(newEntity));
+			changeInsertStatement.setObject(getParamIndex("new_tags"), new PGHStore(newTags));
+
+			changeInsertStatement.setBoolean(getParamIndex("changed_tags"), !newTags.equals(currentTags));
+			changeInsertStatement.setBoolean(getParamIndex("changed_geom"), true);
+			changeInsertStatement.setBoolean(getParamIndex("changed_members"), newEntity.getType() != EntityType.Node);
+
+			break;
+
+		case Delete:
+			if (existingEntity != null) {
+				changeInsertStatement.setObject(getParamIndex("current_geom"), getGeom(existingEntity));
+				changeInsertStatement.setObject(getParamIndex("current_tags"), new PGHStore(getTags(existingEntity)));
+			}
+
+			changeInsertStatement.setBoolean(getParamIndex("changed_tags"), true);
+			changeInsertStatement.setBoolean(getParamIndex("changed_geom"), true);
+
+			break;
+		}
 	}
 
 
 	public void process(ChangeAction action, Node existingNode, Node newNode) throws SQLException {
 		processCommon(action, existingNode, newNode);
-
-		if (action != ChangeAction.Create && existingNode != null) {
-			changeInsertStatement.setObject(9, getTags(existingNode));
-			changeInsertStatement.setObject(12,
-					new PGgeometry(pointBuilder.createPoint(existingNode.getLatitude(), existingNode.getLongitude())));
-		}
-
-		changeInsertStatement.setObject(11,
-				new PGgeometry(pointBuilder.createPoint(newNode.getLatitude(), newNode.getLongitude())));
-
 		changeInsertStatement.executeUpdate();
 	}
 
 
 	public void process(ChangeAction action, Way existingWay, Way newWay) throws SQLException {
 		processCommon(action, existingWay, newWay);
-
-		if (action != ChangeAction.Create && existingWay != null) {
-			changeInsertStatement.setObject(9, getTags(existingWay));
-
-			PGgeometry linestring = (PGgeometry) existingWay.getMetaTags().get("linestring");
-
-			if (linestring != null) {
-				if (linestring.getGeometry().numPoints() == 1) {
-					// Yes - this happens in OSM data - lines with 1 point...
-					// Need to convert them to POINT geometry.
-					changeInsertStatement.setObject(11, new PGgeometry(linestring.getGeometry().getPoint(0)));
-				} else {
-					changeInsertStatement.setObject(11, linestring);
-				}
-			}
-		}
-
 		changeInsertStatement.executeUpdate();
 	}
 
 
 	public void process(ChangeAction action, Relation existingRelation, Relation newRelation) throws SQLException {
-
+		// TODO
 	}
 
 
-	protected PGHStore getTags(Entity entity) {
-		Map<String, String> tags = new HashMap<String, String>();
-		for (Tag tag : entity.getTags()) {
-			tags.put(tag.getKey(), tag.getValue());
+	protected PGgeometry getGeom(Entity entity) {
+		PGgeometry result = null;
+
+		if (entity.getType() == EntityType.Node) {
+			Node node = (Node) entity;
+			result = new PGgeometry(pointBuilder.createPoint(node.getLatitude(), node.getLongitude()));
+		} else if (entity.getType() == EntityType.Way) {
+			Way way = (Way) entity;
+
+			result = (PGgeometry) way.getMetaTags().get("linestring");
+
+			if (result != null && result.getGeometry().numPoints() == 1) {
+				// Yes - this happens in OSM data - lines with 1 point...
+				// Need to convert them to POINT geometry.
+				result = new PGgeometry(result.getGeometry().getPoint(0));
+			}
 		}
-		return new PGHStore(tags);
+
+		return result;
+	}
+
+
+	protected int getParamIndex(String column) {
+		int index = Arrays.asList(INSERT_COLUMNS).indexOf(column) + 1;
+
+		if (index <= 0) {
+			throw new RuntimeException("param index is " + index + " for column " + column);
+		}
+
+		return index;
+	}
+
+
+	protected Map<String, String> getTags(Entity entity) {
+		Map<String, String> tags = new HashMap<String, String>();
+
+		if (entity != null) {
+			for (Tag tag : entity.getTags()) {
+				tags.put(tag.getKey(), tag.getValue());
+			}
+		}
+
+		return tags;
 	}
 }
