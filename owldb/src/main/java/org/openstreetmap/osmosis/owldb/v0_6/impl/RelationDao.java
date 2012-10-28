b49e9e3b9555c25aca823a82ca3345a2b3fde667
Paweł Paprota <ppawel@fastmail.fm>
2012-10-28 14:13:16 +0100

    Fixed NPE in RelationDAO.

--- owldb/src/main/java/org/openstreetmap/osmosis/owldb/v0_6/impl/RelationDao.java
+++ owldb/src/main/java/org/openstreetmap/osmosis/owldb/v0_6/impl/RelationDao.java
@@ -27,12 +27,12 @@ import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
  * @author Brett Henderson
  */
 public class RelationDao extends EntityDao<Relation> {
-	
+
 	private SimpleJdbcTemplate jdbcTemplate;
 	private EntityFeatureDao<RelationMember, DbOrderedFeature<RelationMember>> relationMemberDao;
 	private RelationMemberMapper relationMemberMapper;
-	
-	
+
+
 	/**
 	 * Creates a new instance.
 	 * 
@@ -43,34 +43,36 @@ public class RelationDao extends EntityDao<Relation> {
 	 */
 	public RelationDao(DatabaseContext dbCtx, ActionDao actionDao) {
 		super(dbCtx.getSimpleJdbcTemplate(), new RelationMapper(), actionDao);
-		
+
 		jdbcTemplate = dbCtx.getSimpleJdbcTemplate();
 		relationMemberMapper = new RelationMemberMapper();
 		relationMemberDao = new EntityFeatureDao<RelationMember, DbOrderedFeature<RelationMember>>(
 				dbCtx.getSimpleJdbcTemplate(), relationMemberMapper);
 	}
-	
-	
+
+
 	private void loadFeatures(long entityId, Relation entity) {
 		entity.getMembers().addAll(relationMemberDao.getAllRaw(entityId));
 	}
-	
-	
+
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public Relation getEntity(long entityId) {
 		Relation entity;
-		
+
 		entity = super.getEntity(entityId);
-		
-		loadFeatures(entityId, entity);
-		
+
+		if (entity != null) {
+			loadFeatures(entityId, entity);
+		}
+
 		return entity;
 	}
-	
-	
+
+
 	/**
 	 * Adds the specified relation member list to the database.
 	 * 
@@ -81,87 +83,90 @@ public class RelationDao extends EntityDao<Relation> {
 	 */
 	private void addMembers(long entityId, List<RelationMember> memberList) {
 		List<DbOrderedFeature<RelationMember>> dbList;
-		
+
 		dbList = new ArrayList<DbOrderedFeature<RelationMember>>(memberList.size());
 
 		for (int i = 0; i < memberList.size(); i++) {
 			dbList.add(new DbOrderedFeature<RelationMember>(entityId, memberList.get(i), i));
 		}
-		
+
 		relationMemberDao.addAll(dbList);
 	}
-	
-	
+
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void addEntity(Relation entity) {
 		super.addEntity(entity);
-		
+
 		addMembers(entity.getId(), entity.getMembers());
 	}
-	
-	
+
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void modifyEntity(Relation entity) {
 		long relationId;
-		
+
 		super.modifyEntity(entity);
-		
+
 		relationId = entity.getId();
 		relationMemberDao.removeList(relationId);
 		addMembers(entity.getId(), entity.getMembers());
 	}
-	
-	
+
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void removeEntity(long entityId) {
 		relationMemberDao.removeList(entityId);
-		
+
 		super.removeEntity(entityId);
 	}
-	
-	
+
+
 	private ReleasableIterator<DbOrderedFeature<RelationMember>> getRelationMembers(String tablePrefix) {
-		
-		FileBasedSort<DbOrderedFeature<RelationMember>> sortingStore =
-			new FileBasedSort<DbOrderedFeature<RelationMember>>(
+
+		FileBasedSort<DbOrderedFeature<RelationMember>> sortingStore = new FileBasedSort<DbOrderedFeature<RelationMember>>(
 				new SingleClassObjectSerializationFactory(DbOrderedFeature.class),
 				new DbOrderedFeatureComparator<RelationMember>(), true);
-		
+
 		try {
 			String sql;
 			SortingStoreRowMapperListener<DbOrderedFeature<RelationMember>> storeListener;
 			RowMapperRowCallbackListener<DbOrderedFeature<RelationMember>> rowCallbackListener;
 			ReleasableIterator<DbOrderedFeature<RelationMember>> resultIterator;
-			
+
 			sql = relationMemberMapper.getSqlSelect(tablePrefix, false, false);
-			
+
 			// Sends all received data into the object store.
 			storeListener = new SortingStoreRowMapperListener<DbOrderedFeature<RelationMember>>(sortingStore);
-			// Converts result set rows into objects and passes them into the store.
+			// Converts result set rows into objects and passes them into the
+			// store.
 			rowCallbackListener = new RowMapperRowCallbackListener<DbOrderedFeature<RelationMember>>(
 					relationMemberMapper.getRowMapper(), storeListener);
-			
-			// Perform the query passing the row mapper chain to process rows in a streamy fashion.
+
+			// Perform the query passing the row mapper chain to process rows in
+			// a streamy fashion.
 			jdbcTemplate.getJdbcOperations().query(sql, rowCallbackListener);
-			
-			// Open a iterator on the store that will release the store upon completion.
-			resultIterator =
-				new StoreReleasingIterator<DbOrderedFeature<RelationMember>>(sortingStore.iterate(), sortingStore);
-			
-			// The store itself shouldn't be released now that it has been attached to the iterator.
+
+			// Open a iterator on the store that will release the store upon
+			// completion.
+			resultIterator = new StoreReleasingIterator<DbOrderedFeature<RelationMember>>(sortingStore.iterate(),
+					sortingStore);
+
+			// The store itself shouldn't be released now that it has been
+			// attached to the iterator.
 			sortingStore = null;
-			
+
 			return resultIterator;
-			
+
 		} finally {
 			if (sortingStore != null) {
 				sortingStore.release();
@@ -177,19 +182,19 @@ public class RelationDao extends EntityDao<Relation> {
 	protected List<FeaturePopulator<Relation>> getFeaturePopulators(String tablePrefix) {
 		ReleasableIterator<DbFeature<RelationMember>> relationMemberIterator;
 		List<FeaturePopulator<Relation>> featurePopulators;
-		
+
 		featurePopulators = new ArrayList<FeaturePopulator<Relation>>();
-		
+
 		// Get the way nodes for the selected entities.
 		relationMemberIterator = new UpcastIterator<DbFeature<RelationMember>, DbOrderedFeature<RelationMember>>(
 				getRelationMembers(tablePrefix));
-		
-		// Wrap the way node source into a feature populator that can attach them to their
+
+		// Wrap the way node source into a feature populator that can attach
+		// them to their
 		// owning ways.
-		featurePopulators.add(
-				new FeaturePopulatorImpl<Relation, RelationMember, DbFeature<RelationMember>>(
-						relationMemberIterator, new RelationMemberCollectionLoader()));
-		
+		featurePopulators.add(new FeaturePopulatorImpl<Relation, RelationMember, DbFeature<RelationMember>>(
+				relationMemberIterator, new RelationMemberCollectionLoader()));
+
 		return featurePopulators;
 	}
 }
